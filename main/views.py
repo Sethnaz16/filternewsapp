9c3dd9baac7f738db2c494bf18be55d31fa34640
admin <admin@admins-MacBook-Pro.local>
2017-11-26 03:28:16 -0500

    Added case empty list label

--- main/views.py
+++ main/views.py
@@ -72,67 +72,83 @@ def analyze(request):
     
     return render(request, 'main/results.html', {'data': data, 'id': id, 'labels': labels})
 
+# remove duplcated objects and return
+def remove_dups(json_data):
+    unique_data = { each['label'] : each for each in json_data }.values()
+    return unique_data
+
+# attach each description and return
 def label_desc(query_result):
     desc_label = []
     result = {
         'label': '',
         'desc': ''
     }
-    for query in query_result:
-        if query.label == 'conspiracy':
-            result['label'] = 'conspiracy'  
-            result['desc'] = ('Hate Speech:\tSources that are well-known promoters of kooky conspiracy theories.' + 
-                             ' Ex: 9/11 conspiracies, chem-trails, lizard people in the sewer systems,' + 
-                             ' birther rumors, flat earth ‘theory,’ fluoride as mind control, vaccines as mind control etc')
-        if query.label == 'fake':  
-            result['label'] = 'fake'
-            result['desc'] = ('Fake News:\tSources that entirely fabricate information, disseminate'+ 
-                             'deceptive content, and/or grossly distort actual news reports.')
-        if query.label == 'unreliable': 
-            result['label'] = 'unreliable'
-            result['desc'] = 'Proceed With Caution:\tSources that have been flagged but not yet analyzed.'
-        if query.label == 'hate':  
-            result['label'] = 'hate'
-            result['desc'] = ('Hate Speech:\tSources that actively promote racism, misogyny, homophobia, and other' + 
-                             'forms of discrimination.')
-        if query.label == 'junksci':  
-            result['label'] = 'junksci'
-            result['desc'] = ('Junk Science:\tSources that promote pseudoscience, metaphysics, naturalistic'+ 
-                             'fallacies, and other scientifically dubious claims.')
-        if query.label == 'satire':  
-            result['label'] = 'satire'
-            result['desc'] = ('Satire:\tSources that use humor, irony, exaggeration, ridicule, and false '+
-                             'information to comment on current events.')
-        if query.label == 'bias':  
-            result['label'] = 'bias'
-            result['desc'] = ('Extreme Bias:\tSources that come from a particular point of view and '+
-                             'may rely on propaganda, decontextualized information, and opinions distorted '+
-                             ' as facts. ')
-        if query.label == 'rumor':  
-            result['label'] = 'rumor'
-            result['desc'] = ('Rumor Mill:\tSources that traffic in rumors, gossip, innuendo, and '+
-                             'unverified claims.')
-        if query.label == 'state':  
-            result['label'] = 'state'
-            result['desc'] = ('State News:\tSources in repressive states operating under government sanction.')
-        if query.label == 'clickbait':  
-            result['label'] = 'clickbait'
-            result['desc'] = ('Clickbait:\tSources that are well-known promoters of kooky conspiracy '+
-                             'theories. Ex: 9/11 conspiracies, chem-trails, lizard people in the sewer '+
-                             'systems, birther rumors, flat earth ‘theory,’ fluoride as mind control, '+
-                             'vaccines as mind control etc.')
-        if query.label == 'reliable':  
-            result['label'] = 'reliable'
-            result['desc'] = ('This source is reliable')
-        if query.label == 'political':  
-            result['label'] = 'political'
-            result['desc'] = ('*Note:\tTags like political and credible are being used for two reasons: '+
-                             '1.) they were suggested by viewers of the document or OpenSources and circulate news '+
-                             '2.) the credibility of information and of organizations exists on a continuum, which '+
-                             'this project aims to demonstrate. For now, mainstream news organizations are not '+
-                             'included because they are well known to a vast majority of readers.')
+    # if empty
+    if not query_result:
+        print('Empty')
+        result['label'] = 'unknown'
+        result['desc'] = 'Unknown:\tCannot determine the description for this source'
+    else:
+        for query in query_result:
+            if query.label == 'conspiracy':
+                result['label'] = 'conspiracy'  
+                result['desc'] = ('Hate Speech:\tSources that are well-known promoters of kooky conspiracy theories.' + 
+                                ' Ex: 9/11 conspiracies, chem-trails, lizard people in the sewer systems,' + 
+                                ' birther rumors, flat earth ‘theory,’ fluoride as mind control, vaccines as mind control etc')
+            if query.label == 'fake':  
+                result['label'] = 'fake'
+                result['desc'] = ('Fake News:\tSources that entirely fabricate information, disseminate'+ 
+                                'deceptive content, and/or grossly distort actual news reports.')
+            if query.label == 'unreliable': 
+                result['label'] = 'unreliable'
+                result['desc'] = 'Unreliable:\tProceed With Caution:\tSources that have been flagged but not yet analyzed.'
+            if query.label == 'hate':  
+                result['label'] = 'hate'
+                result['desc'] = ('Hate Speech:\tSources that actively promote racism, misogyny, homophobia, and other' + 
+                                'forms of discrimination.')
+            if query.label == 'junksci':  
+                result['label'] = 'junksci'
+                result['desc'] = ('Junk Science:\tSources that promote pseudoscience, metaphysics, naturalistic'+ 
+                                'fallacies, and other scientifically dubious claims.')
+            if query.label == 'satire':  
+                result['label'] = 'satire'
+                result['desc'] = ('Satire:\tSources that use humor, irony, exaggeration, ridicule, and false '+
+                                'information to comment on current events.')
+            if query.label == 'bias':  
+                result['label'] = 'bias'
+                result['desc'] = ('Extreme Bias:\tSources that come from a particular point of view and '+
+                                'may rely on propaganda, decontextualized information, and opinions distorted '+
+                                ' as facts. ')
+            if query.label == 'rumor':  
+                result['label'] = 'rumor'
+                result['desc'] = ('Rumor Mill:\tSources that traffic in rumors, gossip, innuendo, and '+
+                                'unverified claims.')
+            if query.label == 'state':  
+                result['label'] = 'state'
+                result['desc'] = ('State News:\tSources in repressive states operating under government sanction.')
+            if query.label == 'clickbait':  
+                result['label'] = 'clickbait'
+                result['desc'] = ('Clickbait:\tSources that are well-known promoters of kooky conspiracy '+
+                                'theories. Ex: 9/11 conspiracies, chem-trails, lizard people in the sewer '+
+                                'systems, birther rumors, flat earth ‘theory,’ fluoride as mind control, '+
+                                'vaccines as mind control etc.')
+            if query.label == 'reliable':  
+                result['label'] = 'reliable'
+                result['desc'] = ('This source is reliable')
+            if query.label == 'political':  
+                result['label'] = 'political'
+                result['desc'] = ('*Note:\tTags like political and credible are being used for two reasons: '+
+                                '1.) they were suggested by viewers of the document or OpenSources and circulate news '+
+                                '2.) the credibility of information and of organizations exists on a continuum, which '+
+                                'this project aims to demonstrate. For now, mainstream news organizations are not '+
+                                'included because they are well known to a vast majority of readers.')
+            if not query_result:
+                result['label'] = 'unknown'
+                result['desc'] = 'Unknown:\tCannot determine the description for this article'
             
-        desc_label.append(result)
+    desc_label.append(result)
+    desc_label = remove_dups(desc_label)
         
     return desc_label
 
